48f1dcbb03ae1ca0a26c05a44ca9a5a2f6b2934b
Phan Huu Nam <phnam181102@gmail.com>
2020-12-03 10:39:49 +0700

    update code

---
+++ src/BaiTapChuong6/BaiTap6_5.java
@@ -0,0 +1,23 @@
+package BaiTapChuong6;
+
+import java.util.Scanner;
+
+public class BaiTap6_5 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap so nguyen duong n: ");
+        int n = sc.nextInt();
+        if (n<0) {
+            System.out.println("\nn khong hop le!!!");
+        } else {
+            System.out.println("\n"+n+" co "+DemChuSo(n)+" chu so");
+        }
+    }
+    static int DemChuSo(int n) {
+        if (n < 10) {
+            return 1;
+        } else {
+            return 1 + DemChuSo(n/10);
+        }
+    }
+}
--- src/BaiTapChuong6/BaiTap6_6.java
+++ src/BaiTapChuong6/BaiTap6_6.java
@@ -1,5 +1,13 @@
 package BaiTapChuong6;
 
-public class BaiTap6_6 {
+import java.util.Scanner;
 
+public class BaiTap6_6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Nhap chuoi can dem: ");
+        String s = sc.nextLine();
+        String[] dem = s.split(" ");
+        System.out.println("So tu trong chuoi la: "+(dem.length));
+    }
 }
